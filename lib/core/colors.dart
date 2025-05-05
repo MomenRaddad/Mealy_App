fc7d0f4d668d6e03d9af111f8c8ecf5598340ae7
mo'men raddad <momenraddad12@gmail.com>
2025-05-05 18:24:25 +0300

    remove  space from colors file

--- lib/core/colors.dart
+++ lib/core/colors.dart
@@ -5,22 +5,21 @@ class AppColors {
   static const Color background = Color(0xFFF5F5F5); //Neutral Light
 
   //Brand
-  static const Color primary = Color(0xFF4CAF50);     //Primary Brand
-  static const Color navbarBackground = Color(0xFF4CAF50); 
+  static const Color primary = Color(0xFF4CAF50); //Primary Brand
+  static const Color navbarBackground = Color(0xFF4CAF50);
 
   //Text
   static const Color textPrimary = Color(0xFF212121); //Neutral Dark
   static const Color textSecondary = Color(0xFF9E9E9E); //Neutral Medium
 
   //Accent Colors
-  static const Color accent1 = Color(0xFFFF7043);     //Secondary Button
-  static const Color accent2 = Color(0xFFFFD54F);     //Notifications
+  static const Color accent1 = Color(0xFFFF7043);
+  //Secondary Button
+  static const Color accent2 = Color(0xFFFFD54F); //Notifications
 
   // States
-  static const Color success = Color(0xFF66BB6A);     //Success 
+  static const Color success = Color(0xFF66BB6A); //Success
   static const Color error = Color(0xFFE53935);
-
-     
 }
 
 //Y'all can use it inside the application without any troubles 
