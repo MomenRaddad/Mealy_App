b1a335820f047dbcaceae9f772b1eee32f5c4ed1
mo'men raddad <30956746+momenraddad@users.noreply.github.com>
2025-05-05 15:27:24 +0300

    Update size_extensions.dart

--- lib/utils/size_extensions.dart
+++ lib/utils/size_extensions.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 extension SizeExtension on BuildContext {
   double wp(double pixel) => pixel / MediaQuery.of(this).size.width;
 
-  double hp(double pixel) => pixel / MediaQuery.of(this).size.width;
+  double hp(double pixel) => pixel / MediaQuery.of(this).size.height;
 }
