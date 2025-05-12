83812ac5820ad58f63538fc516cec5afb3e8bd2d
Saif Khalifa <63987779+SaifKhalifa@users.noreply.github.com>
2025-05-12 14:29:12 +0300

    feat: add TaskCard widget for displaying tasks with edit and delete options

---
+++ lib/view/screens/user_screens/home/task_card.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class TaskCard extends StatelessWidget {
+  final String time;
+  final String title;
+  final bool isDone;
+  final VoidCallback? onDelete;
+  final VoidCallback? onEdit;
+
+  const TaskCard({
+    super.key,
+    required this.time,
+    required this.title,
+    required this.isDone,
+    this.onDelete,
+    this.onEdit,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 6),
+      child: ListTile(
+        leading: Icon(
+          isDone ? Icons.check_box : Icons.check_box_outline_blank,
+          color: isDone ? Colors.green : Colors.grey,
+        ),
+        title: Text(
+          title,
+          style: TextStyle(
+            decoration: isDone ? TextDecoration.lineThrough : TextDecoration.none,
+            color: isDone ? Colors.grey : Colors.black,
+          ),
+        ),
+        subtitle: Text(time),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.edit, color: Colors.green),
+              onPressed: onEdit,
+            ),
+            IconButton(
+              icon: const Icon(Icons.delete, color: Colors.red),
+              onPressed: onDelete,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
